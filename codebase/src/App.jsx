1b91f06f96feb85b888ee08b1aa71e9376459fbc
Sreejith K <srejitk@gmail.com>
2022-10-06 17:12:46 +0530

    Feat : Added Header & Routing

--- codebase/src/App.jsx
+++ codebase/src/App.jsx
@@ -1,7 +1,14 @@
 import './App.css';
+import Header from './components/Header';
+import { RouteConfig } from './config/RouteConfig';
 
 function App() {
-  return <div className="App">Inital Setup</div>;
+  return (
+    <div className="App relative">
+      <Header />
+      <RouteConfig />
+    </div>
+  );
 }
 
 export default App;
