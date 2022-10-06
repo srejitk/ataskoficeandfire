28900e12fe2b8a19211e5530f1e558d384f015f7
Sreejith K <srejitk@gmail.com>
2022-10-06 17:01:43 +0530

    Feat : Added Gradient Box

---
+++ codebase/src/components/GradientBox/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const GradientBox = () => {
+  return (
+    <div className="fixed inset-0 h-screen w-screen bg-gradient-to-t from-black to-transparent object-cover"></div>
+  );
+};
+
+export default GradientBox;
