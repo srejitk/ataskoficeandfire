c0f1e392a2294c9e8e72c2002b92850f51318ab9
Sreejith K <srejitk@gmail.com>
2022-10-06 17:01:24 +0530

    Feat : Added Loaders

---
+++ codebase/src/components/Loader/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Loader = () => {
+  return (
+    <div className=" h-10 w-10 translate-y-1/2 translate-x-1/2 animate-ping rounded-full bg-blue-500  "></div>
+  );
+};
+
+export default Loader;
---
+++ codebase/src/components/Skeleton/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Skeleton = () => {
+  return (
+    <div className="m-1 h-[15rem]  w-full min-w-[20rem] max-w-[20rem] animate-pulse cursor-pointer rounded-lg bg-gray-200/50 bg-gradient-to-t from-white to-gray-200 p-10"></div>
+  );
+};
+
+export default Skeleton;
