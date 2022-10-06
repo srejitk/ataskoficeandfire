17be94d05166112f52e11df687f484da5b73dc95
Sreejith K <srejitk@gmail.com>
2022-10-07 02:52:24 +0530

    Style : Added Logo

--- codebase/src/components/Header/index.jsx
+++ codebase/src/components/Header/index.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { IMAGES } from '../../utils/constants';
 
 const Header = () => {
   return (
-    <header className="absolute top-0 z-30 flex h-[5rem] w-full max-w-[screen] items-center justify-start bg-opacity-10 bg-gradient-to-b from-black/70 to-transparent px-10 py-4">
-      <h1 className="text-md font-thin text-white">Game Of Thrones</h1>
+    <header className="absolute top-0 z-30 flex h-[5rem] w-full max-w-[screen] items-center justify-start bg-opacity-10 bg-gradient-to-b from-black/70 to-transparent px-4 py-4 lg:px-10">
+      <div className="flex h-8 w-40 items-center rounded-full bg-white/90 px-4">
+        <img src={IMAGES.LOGO} className=" w-40 object-cover " />
+      </div>
     </header>
   );
 };
--- codebase/src/utils/constants.js
+++ codebase/src/utils/constants.js
@@ -3,4 +3,5 @@ export const IMAGES = {
     'https://res.cloudinary.com/dkqrmlxlg/image/upload/v1665041776/GOT/866625_vuzqfq_bhb5qc.png',
   ICE: 'https://res.cloudinary.com/dkqrmlxlg/image/upload/v1665042261/GOT/3ccef7a0-02e0-4178-8644-b00b66998c5f_jligf0_ixxnab.jpg',
   FIRE: 'https://res.cloudinary.com/dkqrmlxlg/image/upload/v1664999513/GOT/GOT_KL_pwwrni.png',
+  LOGO: 'https://res.cloudinary.com/dkqrmlxlg/image/upload/v1665089711/GOT/series_logos_game-of-thrones_ogfafd-removebg-preview_adwjc1.png',
 };
