03eb81047ea0f024d77a5265d2ec4f2d95891571
Sreejith K <srejitk@gmail.com>
2022-10-06 17:05:20 +0530

    Chore : Added Constants

---
+++ codebase/src/utils/constants.js
@@ -0,0 +1,6 @@
+export const IMAGES = {
+  FIRE_AND_ICE:
+    'https://res.cloudinary.com/dkqrmlxlg/image/upload/v1665041776/GOT/866625_vuzqfq_bhb5qc.png',
+  ICE: 'https://res.cloudinary.com/dkqrmlxlg/image/upload/v1665042261/GOT/3ccef7a0-02e0-4178-8644-b00b66998c5f_jligf0_ixxnab.jpg',
+  FIRE: 'https://res.cloudinary.com/dkqrmlxlg/image/upload/v1664999513/GOT/GOT_KL_pwwrni.png',
+};
